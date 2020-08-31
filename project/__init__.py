25e00e80250df7c1a1f88d7edb1e2b0bfd97d9bb
phoenix1point618 <amila.jayatilaka@gmail.com>
2020-09-01 08:37:54 +1000

    testing

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
@@ -7,7 +9,23 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+app_settings = os.getenv('APP_SETTINGS')  # new
+app.config.from_object(app_settings)      # new
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):  # new
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 class Ping(Resource):
     def get(self):
@@ -18,3 +36,6 @@ class Ping(Resource):
 
 
 api.add_resource(Ping, '/ping')
+
+#import sys
+#print(app.config, file=sys.stderr)
