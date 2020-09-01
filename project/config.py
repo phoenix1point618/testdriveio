5e75a7dbf4920292194875a9f1e73fd5942ba1b8
phoenix1point618 <amila.jayatilaka@gmail.com>
2020-09-01 10:25:00 +1000

    pytest Setup complete

--- project/config.py
+++ project/config.py
@@ -1,9 +1,11 @@
 # project/config.py
 
+import os
 
 class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'
 
 
 class DevelopmentConfig(BaseConfig):
